5ad386e5b03b2196eebc734ed6b8b3aae89c43a8
Unsung Hero <66092015+myrotvorets-team@users.noreply.github.com>
2023-04-15 13:57:03 +0300

    PHP 8.x compatibility

--- inc/class-auth-utils.php
+++ inc/class-auth-utils.php
@@ -7,7 +7,7 @@ use WP_User;
 abstract class Auth_Utils {
 	public static function get_user_by_login_or_email( string $login ): ?WP_User {
 		$user = get_user_by( 'login', $login );
-		if ( false === $user && false !== strpos( $login, '@', 1 ) ) {
+		if ( false === $user && strlen( $login ) >= 3 && false !== strpos( $login, '@', 1 ) ) {
 			$user = get_user_by( 'email', $login );
 		}
 
