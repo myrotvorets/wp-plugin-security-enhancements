50386015efd3721b323025df9de1ae37520ff3bb
Myrotvorets <support@myrotvorets.center>
2024-02-11 12:14:46 +0200

    Drop VENDOR_PATH

--- index.php
+++ index.php
@@ -14,10 +14,7 @@ use Myrotvorets\WordPress\SecEnh\Plugin;
 use Myrotvorets\WordPress\SecEnh\SecEnh_Command;
 
 if ( defined( 'ABSPATH' ) ) {
-	if ( defined( 'VENDOR_PATH' ) ) {
-		/** @psalm-suppress UnresolvableInclude, MixedOperand */
-		require_once constant( 'VENDOR_PATH' ) . '/vendor/autoload.php'; // NOSONAR
-	} elseif ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 		require_once __DIR__ . '/vendor/autoload.php';
 	} elseif ( file_exists( ABSPATH . 'vendor/autoload.php' ) ) {
 		require_once ABSPATH . 'vendor/autoload.php';
